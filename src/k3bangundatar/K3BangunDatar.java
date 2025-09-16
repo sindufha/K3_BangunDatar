cb96a75eea924702ef8aee888342c196e9dcb92e
charis <charis@gmail.com>
2025-09-17 05:58:45 +0700

    tes 3

--- src/k3bangundatar/K3BangunDatar.java
+++ src/k3bangundatar/K3BangunDatar.java
@@ -22,12 +22,35 @@ public class K3BangunDatar {
         s.setsisiB(8);
         s.setsisiC(10);
         
+        System.out.println("Luas dan Keliling Segitiga\n");
         System.out.println("Alas   : " + s.getalas());
         System.out.println("Tinggi : " + s.gettinggi());
         System.out.println("Sisi A : " + s.getsisiA());
         System.out.println("Sisi B : " + s.getsisiB());
         System.out.println("Sisi C : " + s.getsisiC());
-    
+        System.out.println("===================");
+        System.out.println("Luas     : " + s.hitungluas());
+        System.out.println("Keliling : " + s.hitungkeliling());
+        
+        
+        Trapesium t = new Trapesium();
+       
+        t.setsisiAtas(6);
+        t.setsisiBawah(10);
+        t.setsisiMiring1(5);
+        t.setsisiMiring2(7);
+        t.settinggi(8);
+        
+        System.out.println("\nLuas dan Keliling Trapesium\n");
+        System.out.println("Sisi Atas     : " + t.getsisiAtas());
+        System.out.println("Sisi Bawah    : " + t.getsisiBawah());
+        System.out.println("Sisi Miring A : " + t.getsisiMiring1());
+        System.out.println("Sisi Miring B : " + t.getsisiMiring2());
+        System.out.println("Sisi Tinggi   : " + t.gettinggi());
+        System.out.println("===================");
+        System.out.println("luas     : " + t.hitungLuas());
+        System.out.println("Keliling : " + t.hitungkeliling());
+       
     }
-    
+
 }
