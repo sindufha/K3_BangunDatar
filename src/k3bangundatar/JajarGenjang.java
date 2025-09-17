e43bb7c77b0ba9f9da4c7db2bd4b429f01677a7b
ildaff <af032848@gmail.com>
2025-09-17 15:25:43 +0700

    fadli

--- src/k3bangundatar/JajarGenjang.java
+++ src/k3bangundatar/JajarGenjang.java
@@ -9,5 +9,43 @@ package k3bangundatar;
  * @author MyBook Hype AMD
  */
 public class JajarGenjang {
-    
+    double alas;
+    double tinggi;
+    double sisiMiring;
+
+    // Setter (set method)
+    public void setAlas(double alas) {
+        this.alas = alas;
+    }
+
+    public void setTinggi(double tinggi) {
+        this.tinggi = tinggi;
+    }
+
+    public void setSisiMiring(double sisiMiring) {
+        this.sisiMiring = sisiMiring;
+    }
+
+    // Getter (get method)
+    public double getAlas() {
+        return alas;
+    }
+
+    public double getTinggi() {
+        return tinggi;
+    }
+
+    public double getSisiMiring() {
+        return sisiMiring;
+    }
+
+    // Method untuk menghitung luas
+    public double hitungLuas() {
+        return alas * tinggi;
+    }
+
+    // Method untuk menghitung keliling
+    public double hitungKeliling() {
+        return 2 * (alas + sisiMiring);
+    }
 }
