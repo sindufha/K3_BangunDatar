cea882443fabae5f07098f6f93dd2e28a84c2274
charis <charis@gmail.com>
2025-10-08 10:17:33 +0700

    tambah 4

--- src/k3bangundatar/K3BangunDatar.java
+++ src/k3bangundatar/K3BangunDatar.java
@@ -5,11 +5,6 @@ public class K3BangunDatar {
 
 
     public static void main(String[] args) {
-        Segitiga s = new Segitiga(6, 4, 5, 5, 6);
-        
-        s.hitungLuas();
-        s.hitungKeliling();
-        s.tampilkan();
     }
 
 }
