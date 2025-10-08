71e3dd719251c206edcae5e86b8f4b61f66b52a6
sindufha <sindufha215@gmail.com>
2025-10-08 10:41:06 +0700

    Merge origin/master
    # Conflicts:
    #	src/k3bangundatar/BangunDatar.java
    #	src/k3bangundatar/Main.java

--- src/k3bangundatar/Main.java
+++ src/k3bangundatar/Main.java
@@ -5,7 +5,15 @@ public class Main {
     
     
     public static void main(String[] args) {
-      
+      Segitiga s = new Segitiga(6, 8, 5, 7, 9);
+            
+
+            s.hitungLuas();
+            s.hitungKeliling();
+            s.tampilkan();
+           
+          
+        }
     }
 
-}
+
