8a03c65e674658ed41a087a904207d4912b2bb30
sindufha <sindufha215@gmail.com>
2025-10-14 13:48:15 +0700

    final

--- src/k3bangundatar/Main.java
+++ src/k3bangundatar/Main.java
@@ -6,6 +6,11 @@ public class Main {
     
     public static void main(String[] args) {
         
+        LayangLayang layang = new LayangLayang(10, 5, 10, 5);
+        layang.getLuas();
+        layang.getKeliling();
+        layang.tampilkan();
+        
         JajarGenjang jjg = new JajarGenjang(10,20,30); 
         jjg.getLuas();
         jjg.getKeliling();
@@ -16,6 +21,11 @@ public class Main {
         p.Keliling();
         p.tampilkan();
        
+        
+        PersegiPanjang pp = new PersegiPanjang(10, 20);
+        pp.Luas();
+        pp.Keliling();
+        pp.tampilkan();
         
         
       BelahKetupat bk = new BelahKetupat(10, 10, 20);
